f59452db8a761f61129f0c0fffd150376f0720b1
Andrey Demidenko <worm2fed@mail.ru>
2018-02-17 00:32:04 +0300

    disable garbarge includes

--- backend/settingsdev.py
+++ backend/settingsdev.py
@@ -4,5 +4,5 @@ DEBUG = True
 INTERNAL_IPS = ['127.0.0.1']
 ALLOWED_HOSTS += INTERNAL_IPS
 ALLOWED_HOSTS.append('localhost')
-INSTALLED_APPS.append('debug_toolbar')
-MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+# INSTALLED_APPS.append('debug_toolbar')
+# MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
