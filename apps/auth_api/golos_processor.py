349b9cd5cdf6c35a07e8bc21d69295cd619aecc4
Andrey Demidenko <worm2fed@mail.ru>
2018-02-17 14:54:02 +0300

    start to create tools for posting to golos

--- apps/auth_api/golos_processor.py
+++ apps/auth_api/golos_processor.py
@@ -1,9 +1,46 @@
 from django.core.management import BaseCommand
+from steem import Steem
+
+from apps.auth_api.models import Post, Tag
+from backend import settings
 
 
 class GolosCommand(BaseCommand):
     """
     Class to handle interaction with golos blockchain, like posting
     """
+
     def handle(self, *args, **options):
-        pass
+        posts = Post.objects.filter(is_published=False)
+
+        # todo add url
+        meta = {'app': 'URL'}
+        # Send to Golos all not published posts
+        for post in posts:
+            if post.project is None:
+                post = post.worker
+            else:
+                post = post.project
+
+            steem = Steem(post.master_node)
+            steem.wallet.setKeys(settings.ITSPHERE_POSTING_KEY)
+            # Get post tags
+            tags = post.tags
+            # Mark post with ITSphere tag
+            itsphere_tag = Tag.objects.get_or_create(name='itsphere')[0]
+            if itsphere_tag not in tags:
+                tags.append(itsphere_tag)
+            try:
+                if settings.POST_TO_BLOCKCHAIN:
+                    steem.post(
+                        post.title,
+                        post.post.body,
+                        'ITSphere',
+                        post.permlink, meta,
+                        category="test",
+                        tags=tags
+                    )
+                post.is_published = True
+            except Exception:
+                post.is_published = False
+            post.save()
