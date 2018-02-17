142ed74c768834ee5898b678a1dce577a72eb4de
Andrey Demidenko <worm2fed@mail.ru>
2018-02-17 14:52:55 +0300

    write models

--- apps/auth_api/models.py
+++ apps/auth_api/models.py
@@ -2,6 +2,8 @@ from django.core.exceptions import ValidationError
 from django.db import models
 from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.utils.text import slugify
+from transliterate import translit
 
 
 class UserManager(BaseUserManager):
@@ -29,8 +31,6 @@ class UserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     username = models.CharField(max_length=200, unique=True, null=True)
     email = models.EmailField('email address', unique=True, blank=False, null=False)
-    fio = models.CharField(max_length=200, unique=False, null=True)
-    avatar = models.ImageField(upload_to='avatars/', null=True, blank=True, max_length=500)
     created_at = models.DateTimeField(auto_now_add=True, blank=True)
 
     is_active = models.BooleanField('active', default=True)
@@ -51,7 +51,7 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.email
 
     def get_full_name(self):
-        return '{}: {}'.format(self.fio, self.email)
+        return '{}: {}'.format(self.username, self.email)
 
     def get_short_name(self):
         return self.email
@@ -62,25 +62,61 @@ class User(AbstractBaseUser, PermissionsMixin):
             return self.avatar.url
 
 
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return str(self.name)
+
+
 class Project(models.Model):
     """
     This model represents post about project
     """
+    title = models.CharField(max_length=200, blank=False)
+    description = models.TextField(blank=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    is_for_team = models.BooleanField(default=False)
+    github_link = models.URLField(blank=True)
+    gitlab_link = models.URLField(blank=True)
+    bitbucket_link = models.URLField(blank=True)
+    golos_link = models.URLField(blank=False)
+
+    @property
+    def body(self):
+        """
+        Helper to get post body for Golos posting
+        :return: string
+        """
+        # TODO: build post body
+        return self.description
 
 
 class Worker(models.Model):
     """
     This model represents post about worker
     """
+    name = models.CharField(max_length=200, blank=False)
+    about = models.TextField(blank=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    is_team = models.BooleanField(default=False)
+    golos_link = models.URLField(blank=False)
 
+    @property
+    def title(self):
+        """
+        Helper to get worker name as title for posting to Golos
+        """
+        return self.name
 
-class Team(models.Model):
-    """
-    This model represents team, working on project
-    """
-    project = models.ForeignKey('Project', on_delete=models.CASCADE)
-    worker = models.ForeignKey('User', on_delete=models.CASCADE)
-    joined_at = models.DateTimeField(auto_now_add=True, blank=True)
+    @property
+    def body(self):
+        """
+        Helper to get post body for Golos posting
+        :return: string
+        """
+        # TODO: build post body
+        return self.body
 
 
 class Post(models.Model):
@@ -90,11 +126,45 @@ class Post(models.Model):
     """
     project = models.ForeignKey('Project', on_delete=models.CASCADE, blank=True, null=True)
     worker = models.ForeignKey('Worker', on_delete=models.CASCADE, blank=True, null=True)
-    status = models.BooleanField(default=False)
+    is_published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True, null=True)
+    updated_at = models.DateTimeField(auto_now=True, null=True)
+    permlink = models.SlugField(unique=True, null=True, max_length=255)
+
+    @property
+    def post(self):
+        """
+        Helper to get post instance - project or worker profile
+        """
+        if self.project is None:
+            return self.worker
+        else:
+            return self.project
 
     def save(self, *args, **kwargs):
+        # Check iff only one link provided
         if self.project is None and self.worker is None:
             raise ValidationError("You have to link post to project or profile")
         if self.project is not None and self.worker is not None:
             raise ValidationError("You can specify only one of project or profile links")
-        super(Post, self).save(*args, **kwargs)
+
+        # Check is permlink was set
+        if not self.permlink:
+            slug = slugify(translit(self.post.title, 'ru', reversed=True))
+            unique_slug = slug
+
+            num = 1
+            # Prepare slug
+            while self.objects.filter(permlink=unique_slug).exists():
+                unique_slug = '{}-{}'.format(slug, num)
+                num += 1
+            self.permlink = unique_slug
+        super().save()
+
+    @property
+    def tags(self):
+        """
+        Helper to get post tags
+        :return: list
+        """
+        return [t.name for t in self.post.tags.all()]
