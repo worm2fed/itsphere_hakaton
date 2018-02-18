8a1a36cb92aca11246dfd8de20e9d4d933429108
Andrey Demidenko <worm2fed@mail.ru>
2018-02-18 16:01:55 +0300

    cron script fix

--- task_to_cron.sh
+++ task_to_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SHELL=/bin/bash
-*/6 * * * * source /home/bbx/webapps/itsphere/env/bin/activate && /home/bbx/webapps/itsphere/itsphere_hakaton/manage post > /dev/null
+source /home/bbx/webapps/itsphere/env/bin/activate && /home/bbx/webapps/itsphere/itsphere_hakaton/manage post > /dev/null
