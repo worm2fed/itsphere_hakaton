91ced54b90f8b5eaa7def1dd3b761f6170cee793
Andrey Demidenko <worm2fed@mail.ru>
2018-02-18 16:09:17 +0300

    small fixes

--- task_to_cron.sh
+++ task_to_cron.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 SHELL=/bin/bash
-source /home/bbx/webapps/itsphere/env/bin/activate && /home/bbx/webapps/itsphere/itsphere_hakaton/manage post > /dev/null
+source /home/bbx/webapps/itsphere/env/bin/activate && /home/bbx/webapps/itsphere/itsphere_hakaton/manage post >> /home/bbx/webapps/itsphere/itsphere_hakaton/posting.log
